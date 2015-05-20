5a78350a980d44d73e0fa9d58318f00b68596243
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-05-20 14:42:06 +0200

    Add share view controller

---
+++ Gulps/Support/AnimatedShareButton.swift
@@ -0,0 +1,191 @@
+//
+//  AnimatedShareView.swift
+//  Gulps
+//
+//  Created by Andrea Mazzini on 20/05/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+import CoreGraphics
+import QuartzCore
+import UIKit
+
+class AnimatedShareButton: UIButton {
+
+    var box: CAShapeLayer! = CAShapeLayer()
+    var leftShoulder: CAShapeLayer! = CAShapeLayer()
+    var rightShoulder: CAShapeLayer! = CAShapeLayer()
+    var arrowBody: CAShapeLayer! = CAShapeLayer()
+
+    let boxStrokeStart: CGFloat = 0.18
+    let boxStrokeEnd: CGFloat = 0.82
+
+    let arrowStrokeStart: CGFloat = 0.028
+    let arrowStrokeEnd: CGFloat = 0.111
+
+    let shoulderStrokeStart: CGFloat = 0.53
+    let shoulderStrokeEnd: CGFloat = 1
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        let strokeWidth = CGFloat(1.5)
+
+        box.path = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, frame.size.width - strokeWidth / 2, frame.size.height * 0.2)
+            CGPathAddLineToPoint(path, nil, strokeWidth / 2, frame.size.height * 0.2)
+            CGPathAddLineToPoint(path, nil, strokeWidth / 2, frame.size.height - strokeWidth / 2)
+            CGPathAddLineToPoint(path, nil, frame.size.width - strokeWidth / 2, frame.size.height - strokeWidth / 2)
+            CGPathAddLineToPoint(path, nil, frame.size.width - strokeWidth / 2, frame.size.height * 0.2)
+            CGPathAddLineToPoint(path, nil, strokeWidth / 2, frame.size.height * 0.2)
+            return path
+            }()
+
+        arrowBody.path = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, frame.size.width * 0.5 - strokeWidth / 2, 0)
+            CGPathAddLineToPoint(path, nil, frame.size.width * 0.5 - strokeWidth / 2, frame.size.width * 0.7 - strokeWidth / 2)
+            return path
+            }()
+
+        leftShoulder.path = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, frame.size.width * 0.15, -frame.size.height * 0.15)
+            CGPathAddLineToPoint(path, nil, 0, 0)
+            CGPathAddLineToPoint(path, nil, 0, frame.size.width * 0.2 - strokeWidth / 2)
+            return path
+            }()
+
+        rightShoulder.path = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path, nil, -frame.size.width * 0.15, -frame.size.height * 0.15)
+            CGPathAddLineToPoint(path, nil, 0, 0)
+            CGPathAddLineToPoint(path, nil, 0, frame.size.width * 0.2 - strokeWidth / 2)
+            return path
+            }()
+
+
+        for layer in [box, arrowBody, leftShoulder, rightShoulder] {
+            layer.fillColor = nil
+            layer.strokeColor = UIColor.whiteColor().CGColor
+            layer.lineWidth = strokeWidth
+            layer.miterLimit = strokeWidth
+            layer.lineCap = kCALineCapRound
+            layer.masksToBounds = true
+
+            let strokingPath = CGPathCreateCopyByStrokingPath(layer.path, nil, strokeWidth, kCGLineCapRound, kCGLineJoinMiter, strokeWidth)
+
+            layer.bounds = CGPathGetPathBoundingBox(strokingPath)
+
+            layer.actions = [
+                "strokeStart": NSNull(),
+                "strokeEnd": NSNull(),
+                "transform": NSNull()
+            ]
+
+            self.layer.addSublayer(layer)
+        }
+
+        self.box.strokeStart = boxStrokeStart
+        self.box.strokeEnd = boxStrokeEnd
+
+        self.leftShoulder.strokeEnd = shoulderStrokeStart
+        self.rightShoulder.strokeEnd = shoulderStrokeStart
+
+        self.box.position = CGPoint(x: frame.size.width * 0.5, y: frame.size.height * 0.5)
+        self.arrowBody.position = CGPoint(x: frame.size.width * 0.5, y: frame.size.height * 0.2)
+        self.leftShoulder.position = CGPoint(x: frame.size.width * 0.425, y: frame.size.height * 0.025)
+        self.rightShoulder.position = CGPoint(x: frame.size.width * 0.575, y: frame.size.height * 0.025)
+    }
+
+    var showsMenu: Bool = true {
+        didSet {
+            var strokeStart = CABasicAnimation(keyPath: "strokeStart")
+            var strokeEnd = CABasicAnimation(keyPath: "strokeEnd")
+
+            // Box
+            if self.showsMenu {
+                strokeStart.toValue = boxStrokeStart
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, -0.4, 0.5, 1)
+
+                strokeEnd.toValue = boxStrokeEnd
+                strokeEnd.duration = 0.5
+                strokeEnd.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, -0.4, 0.5, 1)
+            } else {
+                strokeStart.toValue = 0
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0, 0.5, 1.2)
+
+                strokeEnd.toValue = 1
+                strokeEnd.duration = 0.5
+                strokeEnd.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0, 0.5, 1.2)
+            }
+
+            self.box.ocb_applyAnimation(strokeStart)
+            self.box.ocb_applyAnimation(strokeEnd)
+
+
+            // Arrow body
+            strokeStart = CABasicAnimation(keyPath: "strokeStart")
+
+            if self.showsMenu {
+                strokeStart.toValue = 0
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, -0.4, 0.5, 1)
+            } else {
+                strokeStart.toValue = 1
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0, 0.5, 1.2)
+            }
+
+            self.arrowBody.ocb_applyAnimation(strokeStart)
+
+
+            // Left shoulder
+            strokeStart = CABasicAnimation(keyPath: "strokeStart")
+
+            if self.showsMenu {
+                strokeStart.toValue = 0
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, -0.4, 0.5, 1)
+
+                strokeEnd.toValue = shoulderStrokeStart
+                strokeEnd.duration = 0.5
+                strokeEnd.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, -0.4, 0.5, 1)
+            } else {
+                strokeStart.toValue = shoulderStrokeStart
+                strokeStart.duration = 0.5
+                strokeStart.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0, 0.5, 1.2)
+
+                strokeEnd.toValue = 1
+                strokeEnd.duration = 0.5
+                strokeEnd.timingFunction = CAMediaTimingFunction(controlPoints: 0.25, -0.4, 0.5, 1)
+            }
+
+            self.leftShoulder.ocb_applyAnimation(strokeStart)
+            self.leftShoulder.ocb_applyAnimation(strokeEnd)
+            self.rightShoulder.ocb_applyAnimation(strokeStart)
+            self.rightShoulder.ocb_applyAnimation(strokeEnd)
+        }
+    }
+}
+
+extension CALayer {
+    // Thanks to https://github.com/robb/hamburger-button
+    func ocb_applyAnimation(animation: CABasicAnimation) {
+        let copy = animation.copy() as! CABasicAnimation
+        
+        if copy.fromValue == nil {
+            copy.fromValue = self.presentationLayer().valueForKeyPath(copy.keyPath)
+        }
+        
+        self.addAnimation(copy, forKey: copy.keyPath)
+        self.setValue(copy.toValue, forKeyPath:copy.keyPath)
+    }
+}
--- Gulps/ViewControllers/CalendarViewController.swift
+++ Gulps/ViewControllers/CalendarViewController.swift
@@ -1,12 +1,24 @@
 import UIKit
 import JTCalendar
+import pop
+import UICountingLabel
 
 class CalendarViewController: UIViewController, JTCalendarDataSource {
 
     @IBOutlet weak var calendarMenu: JTCalendarMenuView!
     @IBOutlet weak var calendarContent: JTCalendarContentView!
     @IBOutlet weak var dailyLabel: UILabel!
+    @IBOutlet weak var calendarConstraint: NSLayoutConstraint!
+    @IBOutlet weak var quantityLabelConstraint: NSLayoutConstraint!
+    @IBOutlet weak var daysLabelConstraint: NSLayoutConstraint!
+    @IBOutlet weak var daysLabel: UICountingLabel!
+    @IBOutlet weak var quantityLabel: UICountingLabel!
+    @IBOutlet weak var measureLabel: UILabel!
+    var quantityLabelStartingConstant = 0.0
+    var daysLabelStartingConstant = 0.0
     var calendar: JTCalendar!
+    var showingStats = false
+    var animating = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,22 +27,96 @@ class CalendarViewController: UIViewController, JTCalendarDataSource {
 
         dailyLabel.text = ""
 
-        let font = UIFont(name: "KaushanScript-Regular", size: 16)
+        let animatedButton = AnimatedShareButton(frame: CGRect(x: 0, y: 0, width: 22, height: 22))
+        animatedButton.addTarget(self, action: Selector("presentStats:"), forControlEvents: .TouchUpInside)
+        let button = UIBarButtonItem(customView: animatedButton)
+        self.navigationItem.rightBarButtonItem = button // UIBarButtonItem(image: UIImage(named: "stats-icon"), style: .Plain, target: self, action: Selector("presentStats:"))
+        
+        setupCalendar()
 
-        calendar = JTCalendar()
-        calendar.calendarAppearance.calendar().firstWeekday = 2
-        calendar.calendarAppearance.dayDotRatio = 1.0 / 7.0
-        calendar.menuMonthsView = calendarMenu
-        calendar.contentView = calendarContent
-        calendar.dataSource = self
-        if let font = font {
-            calendar.calendarAppearance.menuMonthTextFont = font
+        quantityLabelStartingConstant = Double(quantityLabelConstraint.constant)
+        quantityLabelConstraint.constant = view.frame.size.height
+
+        daysLabelStartingConstant = Double(daysLabelConstraint.constant)
+        daysLabelConstraint.constant = view.frame.size.height
+    }
+
+    func presentStats(sender: UIBarButtonItem) {
+        if animating == true {
+            return
         }
-        calendarMenu.reloadAppearance()
+
+        animating = true
+        if let button = self.navigationItem.rightBarButtonItem {
+            let animatedButton: AnimatedShareButton = button.customView as! AnimatedShareButton
+            animatedButton.showsMenu = !animatedButton.showsMenu
+        }
+        if (showingStats) {
+            let slideIn = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+            slideIn.springBounciness = 5
+            slideIn.springSpeed = 8
+            slideIn.fromValue = calendarConstraint.constant
+            slideIn.toValue = 0
+            slideIn.removedOnCompletion = true
+            slideIn.beginTime = CACurrentMediaTime() + 0.35
+            calendarConstraint.pop_addAnimation(slideIn, forKey: "slideAway")
+
+            var slideAway = POPBasicAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+            slideAway.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+            slideAway.fromValue = quantityLabelConstraint.constant
+            slideAway.toValue = view.frame.size.height
+            slideAway.removedOnCompletion = true
+            slideAway.beginTime = CACurrentMediaTime() + 0.10
+            quantityLabelConstraint.pop_addAnimation(slideAway, forKey: "slideInQuantity")
+
+            slideAway = POPBasicAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+            slideAway.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+            slideAway.fromValue = daysLabelConstraint.constant
+            slideAway.toValue = view.frame.size.height
+            slideAway.removedOnCompletion = true
+            daysLabelConstraint.pop_addAnimation(slideAway, forKey: "slideInDays")
+            slideAway.completionBlock = { (_, _) in
+                self.showingStats = false
+                self.animating = false
+            }
+        } else {
+            let slideAway = POPBasicAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+            slideAway.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+            slideAway.fromValue = calendarConstraint.constant
+            slideAway.toValue = -view.frame.size.height
+            slideAway.removedOnCompletion = true
+            slideAway.duration = 0.6
+            calendarConstraint.pop_addAnimation(slideAway, forKey: "slideAway")
+
+            var slideIn = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+            slideIn.springBounciness = 5
+            slideIn.springSpeed = 8
+            slideIn.fromValue = quantityLabelConstraint.constant
+            slideIn.toValue = quantityLabelStartingConstant
+            slideIn.removedOnCompletion = true
+            slideIn.beginTime = CACurrentMediaTime() + 0.35
+            quantityLabelConstraint.pop_addAnimation(slideIn, forKey: "slideInQuantity")
+
+            slideIn = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+            slideIn.springBounciness = 5
+            slideIn.springSpeed = 8
+            slideIn.fromValue = daysLabelConstraint.constant
+            slideIn.toValue = daysLabelStartingConstant
+            slideIn.removedOnCompletion = true
+            slideIn.beginTime = CACurrentMediaTime() + 0.50
+            daysLabelConstraint.pop_addAnimation(slideIn, forKey: "slideInDays")
+            slideIn.completionBlock = { (_, _) in
+                self.showingStats = true
+                self.animating = false
+            }
+        }
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+
+        updateStats()
+
         self.calendar.reloadData()
         if let date = calendar.currentDateSelected {
             updateLabelWithDate(date)
@@ -38,6 +124,40 @@ class CalendarViewController: UIViewController, JTCalendarDataSource {
             updateLabelWithDate(NSDate())
         }
     }
+
+    func updateStats() {
+        [daysLabel, quantityLabel].map { $0.format = "%d" }
+        daysLabel.countFromZeroTo(Float(EntryHandler.overallQuantity()))
+        quantityLabel.countFromZeroTo(Float(EntryHandler.daysTracked()))
+        if let unit = UnitsOfMeasure(rawValue: NSUserDefaults.groupUserDefaults().integerForKey(Settings.General.UnitOfMeasure.key())) {
+            let unitName = unit.nameForUnitOfMeasure()
+            measureLabel.text = "\(unitName) drank in the last"
+        }
+    }
+
+    func updateLabelWithDate(date: NSDate!) {
+        if let entry = Entry.entryForDate(date) {
+            if (entry.percentage >= 100) {
+                dailyLabel.text = "Goal Met!"
+            } else {
+                dailyLabel.text = entry.formattedPercentage()
+            }
+        } else {
+            dailyLabel.text = ""
+        }
+    }
+
+    @IBAction func shareAction(sender: AnyObject) {
+        let quantitiy = EntryHandler.overallQuantity()
+        let days = EntryHandler.daysTracked()
+        let text = "Keeping healthy! I drank \(quantitiy) liters of water in the last \(days) days"
+        let items = [text]
+        let activityController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        activityController.excludedActivityTypes = [UIActivityTypeAirDrop, UIActivityTypeAssignToContact, UIActivityTypeAddToReadingList, UIActivityTypePrint, UIActivityTypePostToWeibo, UIActivityTypePostToVimeo, UIActivityTypePostToTencentWeibo]
+        presentViewController(activityController, animated: true, completion: nil)
+    }
+
+    // MARK: - JTCalendarDataSource
 
     func calendar(calendar: JTCalendar!, dataForDate date: NSDate!) -> AnyObject? {
         let entry = Entry.entryForDate(date)
@@ -51,16 +171,21 @@ class CalendarViewController: UIViewController, JTCalendarDataSource {
     func calendar(calendar: JTCalendar!, didSelectDate date: NSDate!) {
         updateLabelWithDate(date)
     }
+}
 
-    func updateLabelWithDate(date: NSDate!) {
-        if let entry = Entry.entryForDate(date) {
-            if (entry.percentage >= 100) {
-                dailyLabel.text = "Goal Met!"
-            } else {
-                dailyLabel.text = entry.formattedPercentage()
-            }
-        } else {
-            dailyLabel.text = ""
+private extension CalendarViewController {
+    private func setupCalendar() {
+        let font = UIFont(name: "KaushanScript-Regular", size: 16)
+
+        calendar = JTCalendar()
+        calendar.calendarAppearance.calendar().firstWeekday = 2
+        calendar.calendarAppearance.dayDotRatio = 1.0 / 7.0
+        calendar.menuMonthsView = calendarMenu
+        calendar.contentView = calendarContent
+        calendar.dataSource = self
+        if let font = font {
+            calendar.calendarAppearance.menuMonthTextFont = font
         }
+        calendarMenu.reloadAppearance()
     }
 }
