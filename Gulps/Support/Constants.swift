69df90fa6e9b4deec5aa516ed1344c93074131bf
Ross Gibson <rossgibson@me.com>
2016-02-02 14:35:47 +0000

    Reverted Awarai Studios local environment changes, restoring the project back to the original Fancy Pixel settings.

--- Gulps/Support/Constants.swift
+++ Gulps/Support/Constants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public enum Constants {
     public static func bundle() -> String {
-        return "com.awarai.gulps.BigGulp"
+        return "it.fancypixel.BigGulp"
     }
 
     public enum WatchContext: Int {
